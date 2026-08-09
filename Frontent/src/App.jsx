ac7f86196bf8756d5d95889947e6b912966d7a8f
RAHUL <rg967029@gmail.com>
2026-08-09 13:47:53 +0545

    navbar

---
+++ Frontent/src/App.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import { Home } from './pages/Home'
+import { About } from './pages/About'
+import { Collection } from './pages/Collection'
+import { Product } from './pages/Product'
+import { Cart } from './pages/Cart'
+import { Login } from './pages/Login'
+import { PlaceOrder } from './pages/PlaceOrder'
+import { Contact } from './pages/Contact'
+import { Order } from './pages/Order'
+import { Navbar } from './components/Navbar'
+
+const App = () => {
+  return (
+    <div className='px-4 sm:px-6 lg:px-8'>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/order' element={<Order />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/collection' element={<Collection />} />
+        <Route path='/product/:productId' element={<Product />} />
+        <Route path='/cart' element={<Cart />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/place-order' element={<PlaceOrder />} />
+      </Routes>
+    </div>
+  )
+}
+
+export default App
